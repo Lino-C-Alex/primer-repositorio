ed39681507a43c87327fbe7f8c196e578e024254
Lino C Alex <alex.xander.lino@gmail.com>
2021-05-02 20:50:15 -0500

    Version hasta git reset

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 console.log("Hola mundo");
 
 /*
-git init: Para indicar que voy a utilizar git en este proyecto
+git init: Para iniciar un nuevo repositorio
+
 git status: muestra los archivos con los que estamos trabajando
+
+git add<file>: para incluir los archivos a git. Ejemplo: git add index.html
+    A: significa que ha sido añadido a el área temporal
+    M: el archivo ha sido modificado
+    U: aún no está subido al repositorio
+git add . : para añadir toda la carpeta al repositorio
+
+git commit: para crear un punto de control de nuestro código
+    -m "mensaje": para añadir un mensaje
+
+git log: para ver todos los commits que hemos creado
+    --oneline: para mostrar los identificadores
+
+git reset --hard (identificador): para devolver la versión a la que indiquemos
 */
 
