849a342457e7e878368ac2c5cdd7cc851ad50365
Lino C Alex <alex.xander.lino@gmail.com>
2021-05-02 21:24:47 -0500

    Repositorio ya concluido

--- app.js
+++ app.js
@@ -17,6 +17,14 @@ git commit: para crear un punto de control de nuestro código
 git log: para ver todos los commits que hemos creado
     --oneline: para mostrar los identificadores
 
-git reset --hard (identificador): para devolver la versión a la que indiquemos
+git reset --hard (identificador): para devolver la versión a la que indiquemos. Para volver a la versión donde nos encontramos antes, solamente colocamos su identificador y volvemos a la versión previa al salto
+
+git remote add origin (direccion del repositorio de github): con esto indicamos a git que ya estamos trabajando de manera remota usando como identificador el url
+    Ejemplo:
+        git remote add origin https://github.com/Lino-C-Alex/primer-repositorio.git
+
+git push -u origin main: sube los archivos al repositorio de github
+
+Para volver a subir los archivos modificados luego de hacerlo la primera vez, tan solo debemos subir los archivos al memoria temporal (git add .), luego añadirlo al registro (git commit -m "identificador"), por último subirlo a github con (git push)
 */
 
